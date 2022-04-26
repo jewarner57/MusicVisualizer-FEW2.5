048e19e3a03d4a1ddfd1ecd19cc3a21d243da1eb
Jonathan Warner <jewarner57@gmail.com>
2022-04-26 13:27:09 -0400

    feat: hide button should hide settings

--- main.js
+++ main.js
@@ -47,7 +47,37 @@ playButton.addEventListener('click', () => {
 })
 
 buttonToggleShow.addEventListener('click', (e) => {
-  
+  let controlBody = document.querySelector('.music-control-body')
+  let controlContainer = document.querySelector('.music-control-container')
+  let title = document.querySelector('.title-bar > p')
+
+  if(controlsVisible) {
+    controlContainer.classList.add('music-control-hidden')
+    title.classList.add('hidden')
+    controlBody.classList.add('hidden')
+    controlsVisible = false
+
+    buttonToggleShow.innerHTML = 
+    `
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#fff">
+      <path d="M15 12c0 1.657-1.343 3-3 3s-3-1.343-3-3c0-.199.02-.393.057-.581 1.474.541 2.927-.882 2.405-2.371.174-.03.354-.048.538-.048 1.657 0 3 1.344 3 3zm-2.985-7c-7.569 0-12.015 6.551-12.015 6.551s4.835 7.449 12.015 7.449c7.733 0 11.985-7.449 11.985-7.449s-4.291-6.551-11.985-6.551zm-.015 12c-2.761 0-5-2.238-5-5 0-2.761 2.239-5 5-5 2.762 0 5 2.239 5 5 0 2.762-2.238 5-5 5z"/>
+      </svg>
+    `
+  }
+  else {
+    controlContainer.classList.remove('music-control-hidden')
+    title.classList.remove('hidden')
+    controlBody.classList.remove('hidden')
+    controlsVisible = true
+
+    buttonToggleShow.innerHTML = 
+    `
+      <svg xmlns="http://www.w3.org/2000/svg" fill="#DBDBDB" width="24" height="24" viewBox="0 0 24 24">
+        <path
+          d="M11.885 14.988l3.104-3.098.011.11c0 1.654-1.346 3-3 3l-.115-.012zm8.048-8.032l-3.274 3.268c.212.554.341 1.149.341 1.776 0 2.757-2.243 5-5 5-.631 0-1.229-.13-1.785-.344l-2.377 2.372c1.276.588 2.671.972 4.177.972 7.733 0 11.985-8.449 11.985-8.449s-1.415-2.478-4.067-4.595zm1.431-3.536l-18.619 18.58-1.382-1.422 3.455-3.447c-3.022-2.45-4.818-5.58-4.818-5.58s4.446-7.551 12.015-7.551c1.825 0 3.456.426 4.886 1.075l3.081-3.075 1.382 1.42zm-13.751 10.922l1.519-1.515c-.077-.264-.132-.538-.132-.827 0-1.654 1.346-3 3-3 .291 0 .567.055.833.134l1.518-1.515c-.704-.382-1.496-.619-2.351-.619-2.757 0-5 2.243-5 5 0 .852.235 1.641.613 2.342z" />
+      </svg>
+    `
+  }
 })
 
 buttonNextSong.addEventListener('click', () => {
@@ -135,9 +165,8 @@ function nextSong() {
 }
 
 function previousSong() {
-  const temp = currentlyPlaying
+  musicPlaylist.unshift(currentlyPlaying)
   currentlyPlaying = musicPlaylist.pop()
-  musicPlaylist.unshift(temp)
 
   loadNewSong()
 }
@@ -177,11 +206,26 @@ function updatePlaylist() {
   // add back in new order
   for (let song of musicPlaylist) {
     let songElem = document.createElement("div")
-
+    
     songElem.classList.add("playlist-song")
     songElem.innerHTML = song.name
 
     playlistElem.prepend(songElem)
+
+    // If a song in the playlist is clicked
+    songElem.addEventListener('click', (e) => {
+      const songName = e.target.innerHTML
+      const songIndex = musicPlaylist.findIndex((song) => song.name === songName)
+      if(songIndex !== -1) {
+        // set it as the current song and play it
+        let temp = currentlyPlaying
+        currentlyPlaying = musicPlaylist[songIndex]
+        musicPlaylist.splice(songIndex, 1)
+        musicPlaylist.unshift(temp)
+
+        loadNewSong()
+      }
+    })
   }
 }
 
